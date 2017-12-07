52a739b2e5dab5a3c2d06f44238a199a0c0699ca
JinkeLiu <jinkel@andrew.cmu.edu>
2017-12-06 23:22:04 -0500

    change analyze

--- AnalyzeButton.go
+++ AnalyzeButton.go
@@ -3,7 +3,9 @@ package main
 import "github.com/murlokswarm/app"
 
 // FileSummary is the component displaying the summary of the input file.
-type AnalyzeButton struct{}
+type AnalyzeButton struct {
+	Info string
+}
 
 // Render returns the HTML describing the FileSummary component content.
 // It contains a link to show how to navigate to an other component (Sf).
@@ -12,6 +14,7 @@ func (p *AnalyzeButton) Render() string {
 <div class="Home">
 	<div class="Example">
 		<h1>Analysis menu</h1>
+		<textarea  placeholder="Please load input files before analysis :)" >{{if .Info}}{{ .Info}}{{end}}</textarea>
 		<button onclick="OnPipelineButtonClick">Run Pipeline</button>
     <button onclick="OnAlignmentButtonClick">Alignment</button>
     <button onclick="OnVariantButtonClick">Identify variant</button>
@@ -23,14 +26,40 @@ func (p *AnalyzeButton) Render() string {
 	`
 }
 
-func PickFile(files []string) {
+//PickFile is called when the users try to do analysis while there are no
+//corresponding files loaded in app
+func PickFile(h *AnalyzeButton, files *[]string, command string) {
 	app.NewFilePicker(app.FilePicker{ // read the files using a file picker
 		MultipleSelection: true,
 		NoDir:             true,
 		NoFile:            false,
 		OnPick: func(filenames []string) {
 			for i := range filenames { // read all the files into the filepaths array
-				files = append(files, filenames[i])
+				*files = append(*files, filenames[i])
+			}
+			// run different analysis according to their tags
+			switch command {
+			case "Pipeline":
+				h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+				app.Render(h)
+				RunPipeline(*files)
+			case "Alignment":
+				h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+				app.Render(h)
+				samFile := OnlyAlign(*files) // alignment function; generate the sam file; return the filepath
+				sam = append(sam, samFile)   // append the sam file path to the list
+			case "Variant":
+				h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+				app.Render(h)
+				vcfFile := OnlyVCF(*files) // call VCF function; generate .vcf file; return the path
+				vcf = append(vcf, vcfFile) // append the vcf file path to the list
+			case "Drug":
+				h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+				app.Render(h)
+				/*
+					txtFile := ClinicalAnaylisis(*files)// function that analyzes the .vcf file
+					resistence = append(resistence, txtFile) // append txt file to the list for result file paths
+				*/
 			}
 			fileSummary := &FileSummary{} // Creates a FileSummary component
 			win.Mount(fileSummary)        // Mounts the FileSummary component into the window context
@@ -38,38 +67,69 @@ func PickFile(files []string) {
 	})
 }
 
-// OnAlignmentButtonClick is called when when the buttton is clicked.
+// OnAlignmentButtonClick is called when when the button is clicked.
 func (h *AnalyzeButton) OnPipelineButtonClick() {
 	if len(fastq) == 0 { // there's no fastq file loaded
-		PickFile(fastq)
+		h.Info = "Please input .fastq files by using Open file (Ctrl+O)"
+		app.Render(h)
+		PickFile(h, &fastq, "Pipeline")
+	} else {
+		h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+		app.Render(h)
+		RunPipeline(fastq)
+		fileSummary := &FileSummary{} // Creates a FileSummary component
+		win.Mount(fileSummary)        // Mounts the FileSummary component into the window context
 	}
-	RunPipeline(fastq)
 }
 
-// OnAlignmentButtonClick is called when when the buttton is clicked.
+// OnAlignmentButtonClick is called when when the button is clicked.
 func (h *AnalyzeButton) OnAlignmentButtonClick() {
 	if len(fastq) == 0 { // there's no fastq file loaded
-		PickFile(fastq)
+		h.Info = "Please input .fastq files by using Open file (Ctrl+O)"
+		app.Render(h)
+		PickFile(h, &fastq, "Alignment")
+	} else {
+		h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+		app.Render(h)
+		samFile := OnlyAlign(fastq)   // alignment function; generate the sam file; return the filepath
+		sam = append(sam, samFile)    // append the sam file path to the list
+		fileSummary := &FileSummary{} // Creates a FileSummary component
+		win.Mount(fileSummary)        // Mounts the FileSummary component into the window context
 	}
-	samFile := OnlyAlign(fastq) // alignment function; generate the sam file; return the filepath
-	sam = append(sam, samFile)  // append the sam file path to the list
-
 }
 
-// OnAlignmentButtonClick is called when when the buttton is clicked.
+// OnAlignmentButtonClick is called when when the button is clicked.
 func (h *AnalyzeButton) OnVariantButtonClick() {
 	if len(sam) == 0 { // there's no sam file loaded
-		PickFile(sam)
+		h.Info = "Please input .sam files by using Open file (Ctrl+O)"
+		app.Render(h)
+		PickFile(h, &sam, "Variant")
+	} else {
+		h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+		app.Render(h)
+		vcfFile := OnlyVCF(sam)       // call VCF function; generate .vcf file; return the path
+		vcf = append(vcf, vcfFile)    // append the vcf file path to the list
+		fileSummary := &FileSummary{} // Creates a FileSummary component
+		win.Mount(fileSummary)        // Mounts the FileSummary component into the window context
 	}
-	/*
-		vcfFile := OnlyVCF(sam)    // call VCF function; generate .vcf file; return the path
-		vcf = append(vcf, vcfFile) // append the vcf file path to the list
-	*/
 }
 
 // OnAlignmentButtonClick is called when when the buttton is clicked.
 func (h *AnalyzeButton) OnClinicalButtonClick() {
-
+	if len(vcf) == 0 { // there's no vcf file loaded
+		h.Info = "Please input .vcf files by using Open file (Ctrl+O)"
+		app.Render(h)
+		PickFile(h, &vcf, "Drug")
+	} else {
+		h.Info = "Running... Please wait patiently for jumping. This may take a few minutes"
+		app.Render(h)
+		/*
+			txtFile := ClinicalAnaylisis(*files)// function that analyzes the .vcf file
+			resistence = append(resistence, txtFile) // append txt file to the list
+		*/
+		fileSummary := &FileSummary{} // Creates a FileSummary component
+		win.Mount(fileSummary)        // Mounts the FileSummary component into the window context
+	}
 }
 
 // OnPreviousButtonClick is called when the "Previous" button is clicked.
